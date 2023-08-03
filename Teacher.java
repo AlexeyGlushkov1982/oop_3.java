2d8057e4cb62d552ff7bc1ed854630a0a1be1969
alex <klepa_82@bk.ru>
2023-08-03 18:49:30 +0300

    add 8

---
+++ Teacher.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Teacher extends User{
+    public Teacher(String firstName, String secondName, String patronymic, LocalDate dateOfBirth) {
+        super(firstName, secondName, patronymic, dateOfBirth);
+    }
+}
