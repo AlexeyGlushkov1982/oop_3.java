5f99264ac89476099b3004c5219eb35bde657e1b
alex <klepa_82@bk.ru>
2023-08-03 18:48:37 +0300

    add 6

---
+++ StudentGroup.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class StudentGroup {
+
+    public StudentGroup(Teacher teacher, List<Student> students) {
+    }
+
+    public Student[] getStudents() {
+        return null;
+    }
+
+}
